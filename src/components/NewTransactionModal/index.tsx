3d2667f184205b874182b08267bf33c7a7917dd8
Vinicius Barbosa <viniciusmbarbosa@hotmail.com>
2022-10-05 09:31:16 -0300

    feat: TransctionType buttons

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
-import { CloseButton, Content, Overlay } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransctionType,
+  TransctionTypeButton,
+} from './styles'
 
 export function NewTransactionModal() {
   return (
@@ -17,6 +23,18 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransctionType>
+            <TransctionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransctionTypeButton>
+
+            <TransctionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransctionTypeButton>
+          </TransctionType>
 
           <button type="submit">Cadastrar</button>
         </form>
